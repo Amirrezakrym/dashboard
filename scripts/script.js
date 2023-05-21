2d3c9d079698bc9915f9a9e42d848501b5ac8297
Amirreza <amirrezakrym@gmail.com>
2023-05-21 17:17:50 +0430

    add main section

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 // variable
+
+    // ;eft side
     let LeftSlid = document.querySelector('.left-side')
     let leftSideButton = document.querySelector('.left-side-button')
     let BackDivLeft = document.querySelector('.back-div-left')
@@ -8,8 +10,12 @@
     let SlideWrapper1 = document.querySelector('.slide-wrapper-1')
     let SlideWrapper2 = document.querySelector('.slide-wrapper-2')
     let followMe = document.querySelector('.follow-me')
+    // right side
+    let RightSideButton = document.querySelector('.right-side-button')
+    let RightSlid = document.querySelector('.right-side')
+    let BackDivRight = document.querySelector('.back-div-right')
 
-    
+    // open and close left side
     leftSideButton.addEventListener('click',function(){
 
     LeftSlid.classList.toggle("open")
@@ -18,6 +24,7 @@
     // button opacity
     hamb.classList.toggle("hamb-opacity")
     felesh.classList.toggle("felesh-opacity")
+
     // logo
     logo.classList.toggle("logo-open")
     // slide-wrapper-1
@@ -30,7 +37,7 @@
 
 
 
-
+    // open and close background left side
     BackDivLeft.addEventListener('click',function(){
 
     LeftSlid.classList.remove("open")
@@ -39,49 +46,24 @@
      // opacity
      hamb.classList.toggle("hamb-opacity")
      felesh.classList.toggle("felesh-opacity")
+
      // logo
      logo.classList.toggle("logo-open")
      // slide-wrapper-1
      SlideWrapper1.classList.toggle("slide-wrapper-1-opacity")
       // slide-wrapper-2
-      SlideWrapper2.classList.toggle("slide-wrapper-2-opacity")
+     SlideWrapper2.classList.toggle("slide-wrapper-2-opacity")
      // follow-me
-    followMe.classList.toggle("follow-me-opacity")
+     followMe.classList.toggle("follow-me-opacity")
 })
 
+    // open and close left side
+    RightSideButton.addEventListener('click',function(){
+    RightSlid.classList.add("open-right-side")
+    BackDivRight.classList.add("close-back-div-right")
+})
 
-
-
-
-//  function OpenSide(data,width){
-    
-//     let blackDiv =document.createElement("div");
-//     blackDiv.classList.add("black-div");
-//     document.body.appendChild(blackDiv);
-
-//     blackDiv.addEventListener("click", function(){
-
-//         close();
-//     })
-
-
-//     let divShow = document.createElement("div");
-//     divShow.style.width = width;
-//     divShow.className ="div-show";
-//     divShow.innerHTML = data;
-//     document.body.appendChild(divShow);
-
-//     setTimeout(() => {
-//         divShow.classList.add("open");
-//     }, 60);
-
-
-//     function close(){
-//        const divShow = Array.from(document.querySelectorAll(".div-show.open")).pop();
-//        const divBlack = Array.from(document.querySelectorAll(".black-div")).pop();
-
-//        divBlack.remove();
-//        divShow.classList.remove("open");
-//     }
-   
-//  }
+    BackDivRight.addEventListener('click',function(){
+        RightSlid.classList.remove("open-right-side")
+        BackDivRight.classList.remove("close-back-div-right")
+})
